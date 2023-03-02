356d0c31a10cc00eca38c4bcf18d9eb8ec311869
andrelake <andre.lake@hotmail.com>
2023-03-02 20:11:43 -0300

    db config

--- src/main/java/com/andrelake/controllers/UserResource.java
+++ src/main/java/com/andrelake/controllers/UserResource.java
@@ -2,15 +2,22 @@ package com.andrelake.controllers;
 
 import com.andrelake.controllers.dto.CreateUserRequest;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
+
+    @GET
+    public Response getUsers() {
+        return Response.ok().build();
+    }
 
     @POST
     public Response createUser(CreateUserRequest request) {
-        return null;
+        return Response.ok(request).build();
     }
 }
